0a0ce5205677b57b8cc82f2dfb6ae106798c5075
joseryandm <joae.ryan.123@gmail.com>
2022-04-26 11:22:22 -0300

    teste

--- script.js
+++ script.js
@@ -1,44 +1,21 @@
-const btn = document.querySelector('#bttSalvar')
-const LOCAL_API_URL = 'http://localhost:3000/api'
+var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(document.getElementById("sheetdb-form")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+        alert("success")
+        resetarDados()
+    });
+});
 
-btn.addEventListener('click', () => {
-    const dados = getDadosForm()
-    enviarDadosParaAPI(dados)
-})
+const txtC = document.getElementById('txtCerto')
+const txtE = document.getElementById('txtErrado')
 
-function getDadosForm() {
-    const inputCidade = document.querySelector('#cidade')
-    const inputBairro = document.querySelector('#bairro')
-    const inputValorAluguel = document.querySelector('#valorAluguel')
-    const inputQuantiIquilinos = document.querySelector('#quantInquilinos')
-    const inputTelefone = document.querySelector('#telefone')
-
-    const dates = {
-        cidade: inputCidade.value,
-        bairro: inputBairro.value,
-        valorAluguel: inputValorAluguel.value,
-        quantInquilinos: inputQuantiIquilinos.value,
-        telefone: inputTelefone.value
-    }
-    return dates
-}
-
-async function enviarDadosParaAPI(dates) {
-    try {
-        const resposta = await fetch('https://sheetdb.io/api/v1/jc9mvvkn54345', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dates)
-        })
-        if (resposta.status === 201) {
-            limparCampos()
-        } else {
-            console.log('Erro ao adicionar dados')
-        }
-    } catch (erro) {
-        console.error(erro)
-    }
+function resetarDados() {
+    txtC.value = '';
+    txtE.value = '';
 }
